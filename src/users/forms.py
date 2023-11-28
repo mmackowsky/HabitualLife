dc7eba739388b87cebc14866e70245ac9307501d
mmackowsky <maciostah@gmail.com>
2023-11-28 11:32:29 +0100

    feat(forms): update fields

--- src/users/forms.py
+++ src/users/forms.py
@@ -4,13 +4,13 @@ from django.contrib.auth.models import User
 
 
 class SignupForm(UserCreationForm):
+    username = forms.CharField(label="username", min_length=8, max_length=30)
     first_name = forms.CharField(label="First name", min_length=2, max_length=50)
     last_name = forms.CharField(label="Last name", max_length=100)
-    username = forms.CharField(label="Username", min_length=5, max_length=150)
     email = forms.EmailField(label="Email")
     password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
     password2 = forms.CharField(label="Confirm password", widget=forms.PasswordInput)
 
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = ["username", "first_name", "last_name", "email", "password1", "password2"]
