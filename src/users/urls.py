6074b09c847907e93cac3d20ddecaeaf047d551f
mmackowsky <maciostah@gmail.com>
2023-11-29 12:40:52 +0100

    feat(urls): add paths in urls

--- src/users/urls.py
+++ src/users/urls.py
@@ -5,11 +5,28 @@ from .views import (
     LoginView,
     LogoutView,
     SignupView,
+    ActivateView,
+    ResetPasswordView
 )
 
 urlpatterns = [
     path("signup/", SignupView.as_view(), name="signup"),
-    # path("signup/<uidb64>/<token>/activate", ActivateView.as_view(), name="activate"),
+    path("signup/<uidb64>/<token>/activate", ActivateView.as_view(), name="activate"),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path("password-reset/", ResetPasswordView.as_view(), name="password-reset"),
+    path(
+        "password-reset-confirm/<uidb64>/<token>",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="users/password_reset_confirm.html"
+        ),
+        name="password-reset-confirm",
+    ),
+    path(
+        "password-reset-complete/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="users/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
 ]
