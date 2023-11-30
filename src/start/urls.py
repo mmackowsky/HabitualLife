0e5cd2c2cb65b4c607feb922940e488e02dfdd8e
mmackowsky <maciostah@gmail.com>
2023-11-30 11:51:13 +0100

    feat(urls): add main template path

--- src/start/urls.py
+++ src/start/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import start_view
+from .views import start_view, main_view
 
 
 urlpatterns = [
     path('', start_view, name="start"),
+    path('main/', main_view, name='main'),
 ]
