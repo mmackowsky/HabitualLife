4ad893044abb98a10f6583a62cc3d94345595c13
mmackowsky <maciostah@gmail.com>
2023-11-30 11:51:43 +0100

    feat(views): add main_view

--- src/start/views.py
+++ src/start/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 def start_view(request):
     return render(request, 'start.html')
+
+
+def main_view(request):
+    return render(request, 'main.html')
+
