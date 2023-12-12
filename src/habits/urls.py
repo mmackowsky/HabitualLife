6137d807c7aba2f1d26bae53ae7c7287e331022f
mmackowsky <maciostah@gmail.com>
2023-12-12 15:35:48 +0100

    feat(urls): add new path

--- src/habits/urls.py
+++ src/habits/urls.py
@@ -1,8 +1,9 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from .views import CategoryAddView
+from .views import CategoryAddView, HabitAddView
 
 urlpatterns = [
     path("add_category/", CategoryAddView.as_view(), name="add-category"),
+    path("add_habit/", HabitAddView.as_view(), name="add-habit"),
 ]
