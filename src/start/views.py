fe2874687be0d33d643cc92b32fb1b1903769a73
mmackowsky <maciostah@gmail.com>
2023-12-13 13:13:06 +0100

    feat(views): update view

--- src/start/views.py
+++ src/start/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
 
 
-# Create your views here.
-def start_view(request):
-    return render(request, 'start.html')
-
-
 def main_view(request):
-    return render(request, 'main.html')
-
+    if request.user.is_authenticated:
+        return render(request, "main.html")
+    else:
+        return render(request, "start.html")
