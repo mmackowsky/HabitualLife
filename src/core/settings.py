23fc4c39454b42d2080ad4c2e9ae2f48fd3c067b
mmackowsky <maciostah@gmail.com>
2023-12-15 12:37:03 +0100

    feat(settings): add default profile image path

--- src/core/settings.py
+++ src/core/settings.py
@@ -137,6 +137,8 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "media/"
 
+DEFAULT_PROFILE_IMAGE = "user_profile/default.jpg"
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = "static/"
