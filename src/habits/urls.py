f68177be7f7edbf944ffd749432401e96eecdab7
mmackowsky <maciostah@gmail.com>
2023-12-18 15:49:26 +0100

    feat(urls): update urls

--- src/habits/urls.py
+++ src/habits/urls.py
@@ -1,9 +1,21 @@
-from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from .views import CategoryAddView, HabitAddView
+from .views import (
+    CategoryAddView,
+    CategoryDeleteView,
+    CategoryListView,
+    CategoryUpdateView,
+    HabitAddView,
+)
 
 urlpatterns = [
-    path("add_category/", CategoryAddView.as_view(), name="add-category"),
+    path("categories/", CategoryListView.as_view(), name="categories"),
+    path("categories/add/", CategoryAddView.as_view(), name="add-category"),
+    path("categories/edit/", CategoryUpdateView.as_view(), name="edit-category"),
+    path(
+        "categories/delete/<int:pk>",
+        CategoryDeleteView.as_view(),
+        name="delete-category",
+    ),
     path("add_habit/", HabitAddView.as_view(), name="add-habit"),
 ]
