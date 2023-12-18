851e39c8e50fd659ca47e36da467443e7da7f271
mmackowsky <maciostah@gmail.com>
2023-12-18 15:50:16 +0100

    feat(views): refactor views

--- src/habits/views.py
+++ src/habits/views.py
@@ -1,9 +1,12 @@
 import logging
+from typing import Any
 
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db import IntegrityError
+from django.db.models import QuerySet
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
@@ -11,30 +14,55 @@ from .forms import CategoryForm, HabitForm
 from .models import Category, Habit
 
 
+class CategoryListView(LoginRequiredMixin, ListView):
+    model = Category
+    template_name = "habits/categories.html"
+    context_object_name = "categories"
+
+    def get_queryset(self) -> QuerySet[Category]:
+        queryset = Category.objects.filter(user=self.request.user.profile)
+        return queryset
+
+
 class CategoryAddView(LoginRequiredMixin, CreateView):
     model = Category
     fields = ["name"]
-    template_name = "habits/add_category.html"
+    template_name = "habits/categories.html"
+    success_url = reverse_lazy("categories")
 
     # Add category "folder" for habits.
-    def form_valid(self, form):
-        form.instance.user = self.request.user.profile
-        return super().form_valid(form)
+    def form_valid(self, form: CategoryForm) -> HttpResponseRedirect:
+        try:
+            form.instance.user = self.request.user.profile
+            result = super().form_valid(form)
+            messages.success(self.request, "Category added successfully.")
+            return result
+        except IntegrityError:
+            messages.error(self.request, "Category already exists.")
+            return HttpResponseRedirect(reverse_lazy("categories"))
 
 
 class CategoryDeleteView(LoginRequiredMixin, DeleteView):
     model = Category
-    form_class = CategoryForm
+    template_name = "habits/delete_category.html"
+    success_url = reverse_lazy("categories")
 
     # Delete added category within all habits.
+    def get_queryset(self) -> Any:
+        return super().get_queryset().filter(user=self.request.user.profile)
+
+    def form_valid(self, form) -> HttpResponseRedirect:
+        messages.success(self.request, "Category deleted successfully!")
+        return super().form_valid(form)
 
 
 class CategoryUpdateView(LoginRequiredMixin, UpdateView):
     model = Category
+    success_url = reverse_lazy("categories")
     form_class = CategoryForm
 
     # Provide change name of category functionality
-    def form_valid(self, form):
+    def form_valid(self, form: CategoryForm) -> HttpResponseRedirect:
         form.instance.user = self.request.user.profile
         return super().form_valid(form)
 
@@ -48,19 +76,23 @@ class HabitListView(LoginRequiredMixin, ListView):
 class HabitAddView(LoginRequiredMixin, CreateView):
     model = Habit
     form_class = HabitForm
+    template_name = "habits/add_habit.html"
 
-    def form_valid(self, form):
-        category_name = self.kwargs["category_name"]
-        form.instance.user = self.request.user.profile
-        category = Category.objects.get(name=category_name)
-        form.instance.category = category
+    success_url = reverse_lazy("add-habit")
 
+    def get_form_kwargs(self):
+        kwargs = super(HabitAddView, self).get_form_kwargs()
+        kwargs["user"] = self.request.user.profile
+        return kwargs
+
+    def form_valid(self, form: HabitForm) -> HttpResponseRedirect:
         messages.success(self.request, "Habit added.")
         return super().form_valid(form)
 
-    def form_invalid(self, form):
+    def form_invalid(self, form) -> HttpResponseRedirect:
+        messages.error(self.request, "Form invalid.")
         logging.error(form.errors)
-        super().form_invalid(form)
+        return super().form_invalid(form)
 
 
 class HabitDeleteView(DeleteView):
@@ -71,7 +103,7 @@ class HabitDeleteView(DeleteView):
     def get_queryset(self):
         return super().get_queryset().filter(user=self.request.user.profile)
 
-    def form_valid(self, form):
+    def form_valid(self, form: HabitForm) -> HttpResponseRedirect:
         self.object.delete()
         messages.success(self.request, "Habit deleted.")
         return HttpResponseRedirect(self.success_url)
