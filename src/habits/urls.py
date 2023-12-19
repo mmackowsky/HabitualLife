08c40ce8ee431d420b58bc313a293528043b1b16
mmackowsky <maciostah@gmail.com>
2023-12-19 16:43:23 +0100

    feat(urls): change paths

--- src/habits/urls.py
+++ src/habits/urls.py
@@ -6,16 +6,20 @@ from .views import (
     CategoryListView,
     CategoryUpdateView,
     HabitAddView,
+    HabitListView,
 )
 
 urlpatterns = [
     path("categories/", CategoryListView.as_view(), name="categories"),
     path("categories/add/", CategoryAddView.as_view(), name="add-category"),
-    path("categories/edit/", CategoryUpdateView.as_view(), name="edit-category"),
+    path(
+        "categories/edit/<int:pk>", CategoryUpdateView.as_view(), name="edit-category"
+    ),
     path(
         "categories/delete/<int:pk>",
         CategoryDeleteView.as_view(),
         name="delete-category",
     ),
-    path("add_habit/", HabitAddView.as_view(), name="add-habit"),
+    path("habits/", HabitListView.as_view(), name="habits"),
+    path("habits/add/", HabitAddView.as_view(), name="add-habit"),
 ]
