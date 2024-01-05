f8fe854cb8ef5183d31736f67a8cde9ebc28bae0
mmackowsky <maciostah@gmail.com>
2024-01-05 19:51:30 +0100

    feat: add new AchievementListView

--- src/achievements/views.py
+++ src/achievements/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .forms import AchievementForm
+from .models import Achievement
+
+
+class AchievementListView(ListView):
+    model = Achievement
+    form_class = AchievementForm
+    template_name = "achievements/achievements.html"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
+        context["achievements"] = Achievement.objects.all()
+        return context
