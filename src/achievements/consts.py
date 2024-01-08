7537acb308d58bd97d67af341f1f9e6a05d101f3
mmackowsky <maciostah@gmail.com>
2024-01-08 11:51:35 +0100

    feat(consts): add file with const

---
+++ src/achievements/consts.py
@@ -0,0 +1,9 @@
+ACHIEVEMENTS = [
+    ('Hello there', 'Add first habit.'),
+    ('Shot at 10!', 'Achieve 10 days success in habits.'),
+    ('Unstoppable for 50 Days', 'Achieve 50 day success in habits.'),
+    ('Centurion', 'Achieve 100 days success in habits.'),
+    ('Disciplined', 'End all planned habits for one day.'),
+    ('Sloth', 'Skip the habit.'),
+    ('First defeat', 'Failed first habit.')
+]
