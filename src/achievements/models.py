e233900c021b24c3dfb1b665c55b589b11c35a1d
mmackowsky <maciostah@gmail.com>
2024-01-16 16:42:35 +0100

    feat(models): delete models Notification

--- src/achievements/models.py
+++ src/achievements/models.py
@@ -7,8 +7,3 @@ class Achievement(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=250)
     user = models.ManyToManyField(Profile)
-
-
-class Notification(models.Model):
-    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    message = models.CharField(max_length=250)
