1660b4f80e069395c67b24ae4964e815fb5ffa53
mmackowsky <maciostah@gmail.com>
2024-01-16 16:43:20 +0100

    app: add new app notifictaions; initial

---
+++ src/notifications/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+from users.models import Profile
+
+
+class Notification(models.Model):
+    STATUS_CHOICES = (("SEEN", "Seen"), ("UNSEEN", "Unseen"))
+
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    message = models.CharField(max_length=250)
+    status = (models.CharField(max_length=6, choices=STATUS_CHOICES, default="UNSEEN"),)
+    create_date = models.DateTimeField(auto_now_add=True)
