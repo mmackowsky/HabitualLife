94813929a4250d630333d85a08b7a0505c19d1ab
mmackowsky <maciostah@gmail.com>
2024-01-17 17:37:11 +0100

    feat(views): add views

--- src/notifications/views.py
+++ src/notifications/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.base import ContextMixin
 
-# Create your views here.
+from .models import Notification
+
+
+class NotificationsListMixin(ContextMixin):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context["notifications"] = Notification.objects.filter(
+            user=self.request.user.profile
+        )
+        return context
+
+
+class NotificationsListView(NotificationsListMixin, ListView):
+    model = Notification
+    template_name = "notifications/notifications.html"
