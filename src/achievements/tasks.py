372040a55f7b7e2682d1e0381b3660d249c5fa03
mmackowsky <maciostah@gmail.com>
2024-01-21 14:19:32 +0100

    feat(tasks): add send notification task

--- src/achievements/tasks.py
+++ src/achievements/tasks.py
@@ -1,10 +1,11 @@
 import datetime
-from abc import ABC, abstractmethod
+from typing import Union
 
-from celery import Task, shared_task
+from celery import shared_task
 from celery.utils.log import get_task_logger
 
 from habits.models import Habit
+from notifications.models import Notification
 from users.models import Profile
 
 from .models import Achievement
@@ -18,23 +19,34 @@ def save_achievement(achievement_name: str, user: Profile, log_text: str) -> Non
     logger.info(log_text)
 
 
+def send_notification(recipient: Union[Profile, int]) -> None:
+    notification_text = "Achievement get!! Congratulations, you can check your achievement in Achievement card."
+
+    if isinstance(recipient, int):
+        recipient = Profile.objects.get(pk=recipient)
+
+    Notification.objects.create(user=recipient, message=notification_text)
+
+
 @shared_task
-def set_first_habit_achievement(self, user: Profile) -> str | None:
+def set_first_habit_achievement(user: Profile) -> str | None:
     if Habit.objects.filter(user=user).count() == 1:
         save_achievement(
             achievement_name="Hello there", user=user, log_text="Hello there - admit"
         )
+        send_notification(recipient=user)
         return "Admitted"
 
 
 @shared_task
-def set_daily_streak_achievement(self, user: Profile) -> str | None:
+def set_daily_streak_achievement(user: Profile) -> str | None:
     habits = Habit.objects.filter(user=user)
     for habit in habits:
         if habit.streak_count == 100:
             save_achievement(
                 achievement_name="Centurion", user=user, log_text="Centurion - admit"
             )
+            send_notification(recipient=user)
             return "Admitted"
         if habit.streak_count == 50:
             save_achievement(
@@ -42,6 +54,7 @@ def set_daily_streak_achievement(self, user: Profile) -> str | None:
                 user=user,
                 log_text="Unstoppable for 50 Days - admit",
             )
+            send_notification(recipient=user)
             return "Admitted"
         if habit.streak_count == 10:
             save_achievement(
@@ -49,11 +62,12 @@ def set_daily_streak_achievement(self, user: Profile) -> str | None:
                 user=user,
                 log_text="Shot at 10! - admit",
             )
+            send_notification(recipient=user)
             return "Admitted"
 
 
 @shared_task
-def set_all_habits_for_day_done_achievement(self, user: Profile) -> str | None:
+def set_all_habits_for_day_done_achievement(user: Profile) -> str | None:
     habits = Habit.objects.filter(user=user).exclude(
         execution_date=datetime.date.today(), active=True
     )
@@ -61,22 +75,24 @@ def set_all_habits_for_day_done_achievement(self, user: Profile) -> str | None:
         save_achievement(
             achievement_name="Disciplined", user=user, log_text="Disciplined - admit"
         )
+        send_notification(recipient=user)
         return "Admitted"
 
 
 @shared_task
-def set_skip_first_habit_achievement(self, user: Profile) -> str | None:
+def set_skip_first_habit_achievement(user: Profile) -> str | None:
     habits = Habit.objects.filter(user=user)
     for habit in habits:
         if habit.skipped_count == 1:
             save_achievement(
                 achievement_name="Sloth", user=user, log_text="Sloth - admit"
             )
+            send_notification(recipient=user)
             return "Admitted"
 
 
 @shared_task()
-def set_fail_first_habit_achievement(self, user: Profile) -> str | None:
+def set_fail_first_habit_achievement(user: Profile) -> str | None:
     habits = Habit.objects.filter(user=user)
     for habit in habits:
         if habit.failed_count == 1:
@@ -85,4 +101,5 @@ def set_fail_first_habit_achievement(self, user: Profile) -> str | None:
                 user=user,
                 log_text="First defeat - admit",
             )
+            send_notification(recipient=user)
             return "Admitted"
