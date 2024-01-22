0753be76046d1b0a9674ef51f81449781e361493
mmackowsky <maciostah@gmail.com>
2024-01-22 10:48:26 +0100

    feat(views): update

--- src/achievements/views.py
+++ src/achievements/views.py
@@ -2,13 +2,11 @@ from django.views.generic import ListView
 
 from notifications.views import NotificationsListMixin
 
-from .forms import AchievementForm
 from .models import Achievement
 
 
 class AchievementListView(ListView, NotificationsListMixin):
     model = Achievement
-    form_class = AchievementForm
     template_name = "achievements/achievements.html"
 
     def get_context_data(self, *, object_list=None, **kwargs):
