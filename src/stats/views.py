e35d6875799519a7bcc0f396286c88ac53858f18
mmackowsky <maciostah@gmail.com>
2024-01-24 10:05:07 +0100

    feat(views): refactor

--- src/stats/views.py
+++ src/stats/views.py
@@ -1,33 +1,43 @@
 import plotly.graph_objs as go
 from django.db.models import Sum
 from django.shortcuts import render
+from django.views import View
 from plotly.offline import plot
 
 from habits.models import Habit
+from notifications.views import NotificationsListMixin
 
 
-def habit_statistics(request):
-    habits = Habit.objects.filter(user=request.user.profile)
+class HabitStatisticsView(View, NotificationsListMixin):
+    def get(self, request):
+        notification_context = self.get_context_data()
 
-    success_count = habits.aggregate(success_count=Sum("success_count"))[
-        "success_count"
-    ]
-    failed_count = habits.aggregate(failed_count=Sum("failed_count"))["failed_count"]
-    skipped_count = habits.aggregate(skipped_count=Sum("skipped_count"))[
-        "skipped_count"
-    ]
+        habits = Habit.objects.filter(user=request.user.profile)
 
-    labels = ["SUCCESS", "FAILED", "SKIPPED"]
-    values = [success_count, failed_count, skipped_count]
+        success_count = habits.aggregate(success_count=Sum("success_count"))[
+            "success_count"
+        ]
+        failed_count = habits.aggregate(failed_count=Sum("failed_count"))[
+            "failed_count"
+        ]
+        skipped_count = habits.aggregate(skipped_count=Sum("skipped_count"))[
+            "skipped_count"
+        ]
 
-    # Tworzenie wykresu słupkowego
-    trace = go.Bar(x=labels, y=values)
-    layout = go.Layout(
-        title="Status Statistics", xaxis=dict(title="Status"), yaxis=dict(title="Count")
-    )
-    fig = go.Figure(data=[trace], layout=layout)
-    plot_div = plot(fig, output_type="div")
+        labels = ["SUCCESS", "FAILED", "SKIPPED"]
+        values = [success_count, failed_count, skipped_count]
 
-    context = {"plot_div": plot_div}
+        # Tworzenie wykresu słupkowego
+        trace = go.Bar(x=labels, y=values)
+        layout = go.Layout(
+            title="Status Statistics",
+            xaxis=dict(title="Status"),
+            yaxis=dict(title="Count"),
+        )
+        fig = go.Figure(data=[trace], layout=layout)
+        plot_div = plot(fig, output_type="div")
 
-    return render(request, "statistics/statistics.html", context)
+        context = {"plot_div": plot_div}
+        context.update(notification_context)
+
+        return render(request, "stats/stats.html", context)
