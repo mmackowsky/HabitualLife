87cb8eef038b2942f72839ca81bdb15d2c4810ca
mmackowsky <maciostah@gmail.com>
2024-01-24 09:44:21 +0100

    feat(settings): update settings

--- src/core/settings.py
+++ src/core/settings.py
@@ -211,11 +211,11 @@ if env("USE_EMAIL"):
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 # AWS settings
-if env("USE_AWS"):
-    AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
-    AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
-    AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME")
+# if env("USE_AWS"):
+#     AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+#     AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
+#     AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
+#     AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME")
 
 # SENTRY
 sentry_sdk.init(
