bc97771a3b53335e7c5e8804c432dca44aa7bd95
mmackowsky <maciostah@gmail.com>
2024-01-24 10:05:44 +0100

    tests(stats): add tests

--- src/stats/tests.py
+++ src/stats/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory, Client
+from django.urls import reverse
+from habits.models import Habit
+from users.factories import ProfileFactory, UserFactory
+from habits.factories import HabitFactory
 
-# Create your tests here.
+
+class HabitStatisticsViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.factory = RequestFactory()
+        self.user = UserFactory(username='testuser', password='testpass')
+        self.client.login(username='testuser', password='testpass')
+        self.profile = ProfileFactory(user=self.user)
+        self.habit = HabitFactory()
+        self.url = reverse('stats')
+        self.response = self.client.get(self.url)
+
+    def test_get_habit_statistics(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'stats/stats.html')
+        self.assertIn('plot_div', self.response.context)
+
+    def test_get_habit_statistics_with_no_habits(self):
+        Habit.objects.all().delete()
+
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'stats/stats.html')
+        self.assertIn('plot_div', self.response.context)
+
+    def test_get_habit_statistics_with_notification(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'stats/stats.html')
+        self.assertIn('notifications', self.response.context)
+        self.assertIn('notifications_number', self.response.context)
