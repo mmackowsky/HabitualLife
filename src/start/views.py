bc57dade9cdd67971b81be0a492bcb3d8b107a3f
mmackowsky <maciostah@gmail.com>
2024-01-28 13:56:02 +0100

    feat(views): update view

--- src/start/views.py
+++ src/start/views.py
@@ -4,5 +4,4 @@ from django.shortcuts import redirect, render
 def main_view(request):
     if request.user.is_authenticated:
         return redirect("habits")
-    else:
-        return render(request, "start.html")
+    return render(request, "start.html")
