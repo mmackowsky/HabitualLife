24d6c95726e7f3a1d439066cdf9576ce55654553
mmackowsky <maciostah@gmail.com>
2024-01-28 13:57:10 +0100

    feat(urls): update

--- src/stats/urls.py
+++ src/stats/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 
 from .views import HabitStatisticsView
 
-urlpatterns = [path("stats/", HabitStatisticsView.as_view(), name="stats")]
+urlpatterns = [
+    path("stats/", HabitStatisticsView.as_view(), name="stats"),
+]
