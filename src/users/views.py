725f5b7321492f52c03287803aeade20a8ec77b8
mmackowsky <maciostah@gmail.com>
2024-01-29 14:27:39 +0100

    feat(views): update

--- src/users/views.py
+++ src/users/views.py
@@ -21,7 +21,7 @@ from django.urls import reverse, reverse_lazy
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.views import View
-from django.views.generic import FormView
+from django.views.generic import FormView, ListView
 
 from core import settings
 from habits.models import Category
