9d91af2e372ad8125980ed6c25379ed3a1d6da36
mmackowsky <maciostah@gmail.com>
2024-02-18 13:44:50 +0100

    settings: update settings

--- src/core/settings.py
+++ src/core/settings.py
@@ -88,14 +88,6 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# if env("ENVIRONMENT") == "ci":
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": "django.db.backends.sqlite3",
-#             "NAME": "ci_db",
-#         }
-#     }
-# else:
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
@@ -106,7 +98,6 @@ DATABASES = {
         "PORT": env("DB_PORT"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
